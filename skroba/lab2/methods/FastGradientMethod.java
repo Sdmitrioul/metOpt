9b206da428f423820a014d447b279cff30b44f40
”Dmitri <dmitri.zd1958@gmail.com>
2021-06-02 19:43:29 +0300

    Make creating minimimum searcher in FastGradientMethod more flexible

--- skroba/lab2/methods/FastGradientMethod.java
+++ skroba/lab2/methods/FastGradientMethod.java
@@ -7,6 +7,8 @@ import skroba.utils.Pair;
 import skroba.utils.QuadraticFunction;
 import skroba.utils.Vector;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.function.Function;
 
 /**
@@ -14,13 +16,14 @@ import java.util.function.Function;
  */
 public class FastGradientMethod extends AbstractGradientIterator {
 	private final static String NAME = "Fast gradient descent";
+	private Class<? extends MinimumSearcher> oneDimSearcherToken = GoldenRatioMethod.class;
 	
 	/**
 	 *	Constructor with standard EPS equals 1e-3.
 	 *
 	 * @param function - given minimizing function.
 	 */
-	public FastGradientMethod(QuadraticFunction function) {
+	public FastGradientMethod(final QuadraticFunction function) {
 		super(NAME, function);
 	}
 	
@@ -30,8 +33,31 @@ public class FastGradientMethod extends AbstractGradientIterator {
 	 * @param function - given minimizing function.
 	 * @param EPS - double value characterizing precession of calculating.
 	 */
-	public FastGradientMethod(QuadraticFunction function, Double EPS) {
+	public FastGradientMethod(final QuadraticFunction function, final Double EPS) {
 		super(NAME, function, EPS);
+	}
+	
+	/**
+	 * Constructor with given EPS and one dim minimum searcher.
+	 *
+	 * @param function - given minimizing function.
+	 * @param EPS - double value characterizing precession of calculating.
+	 * @param oneDimSearcherToken - {@link Class} token of minimum searcher. It must implement {@link MinimumSearcher} interface.
+	 */
+	public FastGradientMethod(final QuadraticFunction function, final Double EPS, final Class<? extends MinimumSearcher> oneDimSearcherToken) {
+		super(NAME, function, EPS);
+		this.oneDimSearcherToken = oneDimSearcherToken;
+	}
+	
+	/**
+	 * Constructor with given one dim minimum searcher.
+	 *
+	 * @param function - given minimizing function.
+	 * @param oneDimSearcherToken - {@link Class} token of minimum searcher. It must implement {@link MinimumSearcher} interface.
+	 */
+	public FastGradientMethod(final QuadraticFunction function, final Class<? extends MinimumSearcher> oneDimSearcherToken) {
+		super(NAME, function);
+		this.oneDimSearcherToken = oneDimSearcherToken;
 	}
 	
 	@Override
@@ -43,14 +69,32 @@ public class FastGradientMethod extends AbstractGradientIterator {
 			return false;
 		}
 		
-		final Function<Double, Vector> oneDimFun = x -> currentValue.getFirst().sum(function.getGradient(currentValue.getFirst()).scalarMul(-x));
-		MinimumSearcher searcher = new GoldenRatioMethod(EPS / 100, STANDARD_DELTA, oneDimFun.andThen(function));
+		final Function<Double, Vector> oneDimFun = x -> currentValue.getFirst()
+				.sum(function.getGradient(currentValue.getFirst()).scalarMul(-x));
+		
+		MinimumSearcher searcher = getMinimumSearcher(oneDimFun);
+		
 		try {
 			final double val = searcher.findMin(0.0, 1.0).getMin();
 			nextValue = new Pair<>(oneDimFun.apply(val), val);
 			return true;
 		} catch (TimeOutException ex) {
 			return false;
+		}
+	}
+	
+	/**
+	 * Function that returns new instance of minimum searcher.
+	 * @param oneDimFun - one dim function for minimizing.
+	 * @return - instance of class implementing {@link MinimumSearcher} interface.
+	 */
+	private MinimumSearcher getMinimumSearcher(Function<Double, Vector> oneDimFun) {
+		try {
+			return oneDimSearcherToken
+					.getConstructor(double.class, double.class, Function.class)
+					.newInstance(EPS / 100, STANDARD_DELTA, oneDimFun.andThen(function));
+		} catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException ex) {
+			throw new RuntimeException("There's no such constructor: " + ex.getMessage());
 		}
 	}
 	
